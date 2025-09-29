18580e09b60f62e4e33b1890c6ae765423270be9
shreyashJ5709 <ajaiswal5709@gmail.com>
2025-09-29 11:48:25 +0000

    Comparison Completed

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,13 @@
+// console.log(2 > 1); // true
+// console.log(2 >= 1); // true
+// console.log(2 < 1); // false 
+// console.log(2 <= 1); // false
+// console.log(2 == 1); // false 
+// console.log(2 != 1); // true
+
+// console.log("2" > 1); // first "2" typeConvert in number then compare 
+// console.log("2" >= 1);
+
+// console.log(null > 0);
+// console.log(null == 0);  these comparisons are avoided.
+// console.log(null >= 0);
