c3643ae4b7eb983654c8007da8ded92e5716e963
shreyashJ5709 <ajaiswal5709@gmail.com>
2025-09-29 11:16:36 +0000

    DataTypes completed.

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat all JS code as newer version.
+
+// alert(2+3); // we are using node.js not a browser.
+
+let name = "Shreyash"; // string
+let age = 20; // number => int, float, double, sort
+let isLoggedIn = true; // boolean
+
+// number => 2 to power 53
+// null => standlone value
+// undefined
+// symbol => unique
+
+console.log(typeof (null)); // object
+console.log(typeof (undefined)); // undefined
+
