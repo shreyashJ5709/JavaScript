36b4bbb36e759bdde4b4f8e81ec5cefdc39f10bd
shreyashJ5709 <ajaiswal5709@gmail.com>
2025-09-29 10:57:52 +0000

    Variable declearation complete.

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14335642;
+var accountPasw = "ghjhghfh";
+let accountEmail = "gjhgjgj@gmail.com";
+
+// accountId = 2; Not Allowed
+console.log(accountId);
+console.table([accountId, accountPasw, accountEmail]); // print in tabular form.
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
