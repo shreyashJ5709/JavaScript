270e62bd392f0b140e6085104d031fe142333f1d
shreyashJ5709 <ajaiswal5709@gmail.com>
2025-09-29 13:53:24 +0000

    Object destructuring complete.

--- 02_Basics/02_objets.js
+++ 02_Basics/02_objets.js
@@ -1,19 +1,36 @@
 // object merging 
 
-const obj1 = {
-    1:"a", 2:"b", 3:"c",
-};
+// const obj1 = {
+//     1:"a", 2:"b", 3:"c",
+// };
 
-const obj2 = {
-    4:"A", 5:"B", 6:"C",
-};
+// const obj2 = {
+//     4:"A", 5:"B", 6:"C",
+// };
 
-const obj3 = {
-    7:"1", 8:"2", 9:"3",
-};
+// const obj3 = {
+//     7:"1", 8:"2", 9:"3",
+// };
 
-// const obj = Object.assign({}, obj1, obj2, obj3);
-const obj = {...obj1, ...obj2, ...obj3}; // spred method.
+// // const obj = Object.assign({}, obj1, obj2, obj3);
+// const obj = {...obj1, ...obj2, ...obj3}; // spred method.
 
-console.log(obj);
-console.log(obj1);
+// console.log(obj);
+// console.log(obj1);
+
+
+// ************************************************   Object Destructuring  ****************************************************************
+
+const course = {
+    name: "JS",
+    fee: 99,
+    courseInstructor: "Hitesh Chaudhary",
+}
+
+// console.log(course.courseInstructor); // 1st - method
+
+const {courseInstructor} = course;
+console.log(courseInstructor); // 2nd - method
+
+const {courseInstructor : instructor} = course;
+console.log(instructor); // 3rd - method
